e09df7d40c4b0747664a15d9e660f302ecd942f3
Alopmel <78558722+Alopmel@users.noreply.github.com>
2024-07-09 00:08:04 +0200

    :star: Fixed next.config

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,11 @@
-module.exports = {
+export default {
     env: {
       BUCKET_NAME: process.env.BUCKET_NAME,
       AWS_ACCESS_KEY_ID: process.env.AWS_ACCESS_KEY_ID,
       AWS_SECRET_ACCESS_KEY: process.env.AWS_SECRET_ACCESS_KEY,
-      AWS_REGION: process.env.AWS_REGION
+      AWS_REGION: process.env.AWS_REGION,
+      NEXT_PUBLIC_USER_POOL_ID: process.env.NEXT_PUBLIC_USER_POOL_ID,
+      NEXT_PUBLIC_USER_POOL_CLIENT_ID: process.env.NEXT_PUBLIC_USER_POOL_CLIENT_ID
     },
     async rewrites() {
       return [
