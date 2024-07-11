7c12ea7798bc5308d6086df857f699b52b36b636
Alopmel <78558722+Alopmel@users.noreply.github.com>
2024-07-11 22:14:52 +0200

    :art: Display none sideNabar on mobile

--- src/app/portal/layout.tsx
+++ src/app/portal/layout.tsx
@@ -1,20 +1,33 @@
-// import Navbar from "@/ui/portal/navbar/navbar";
-
-// export default function Layout({ children }: { children: React.ReactNode }) {
-//   return (
-//     <div>
-//       <Navbar />
-//       <div>{children}</div>
-//     </div>
-//   );
-// }
-
-import HeaderMobile from "@/ui/portal/userprofile/header-mobile"
+// import HeaderMobile from "@/ui/portal/userprofile/header-mobile"
+'use client'
+import { useEffect, useState } from "react";
 import SideNav from "@/ui/portal/userprofile/side-nav";
 export default function Layout({ children }: { children: React.ReactNode }) {
+  const [ isDesktop, setIsDesktop ] = useState<boolean>(false);
+
+  const checkWindowSize = () => {
+    let windowWidth = 0;
+    if (typeof window !== 'undefined') {
+      windowWidth = window.innerWidth;
+    }
+    if (windowWidth >= 1024) { 
+      setIsDesktop(true)
+      console.log('Destop es true ' + isDesktop)
+      console.log('windowWidth ' + windowWidth)
+    } else {
+      setIsDesktop(false)
+      console.log('Destop es false ' + isDesktop)
+      console.log('windowWidth ' + windowWidth)
+    }
+  }
+  // logic when user first load the page
+    useEffect(() => {
+      checkWindowSize();
+    }, [isDesktop])
+  
   return (
     <div className="flex">
-      <SideNav />
+      {isDesktop ? (<SideNav />): ('')}
       <main className="flex-1">
         {/* <HeaderMobile /> */}
         {children}  
