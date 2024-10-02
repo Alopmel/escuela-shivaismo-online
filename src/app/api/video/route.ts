ae7817abc41e99807c540fd2761be1ffa325f31f
Alopmel <78558722+Alopmel@users.noreply.github.com>
2024-10-02 16:53:23 +0200

    a ver si se sube

--- src/app/api/video/route.ts
+++ src/app/api/video/route.ts
@@ -29,3 +29,75 @@ export async function GET() {
   }
 }
 
+
+export async function POST(req: NextRequest) {
+  console.log('Iniciando carga de video');
+  
+  if (!bucketName) {
+    console.error('BUCKET_NAME no está definido en las variables de entorno');
+    return NextResponse.json({ success: false, message: "Error de configuración del servidor" }, { status: 500 });
+  }
+
+  try {
+    const formData = await req.formData();
+    const file = formData.get('file') as File;
+    const folder = formData.get('folder') as string;
+    let customName = formData.get('customName') as string;
+    
+    if (!file || !folder) {
+      return NextResponse.json({ success: false, message: "No se proporcionó archivo o carpeta" }, { status: 400 });
+    }
+
+    const buffer = await file.arrayBuffer();
+
+    // Obtener la lista de objetos en la carpeta
+    const listParams = {
+      Bucket: bucketName,
+      Prefix: folder + '/'
+    };
+    const listResult = await s3.listObjectsV2(listParams).promise();
+
+    // Determinar el próximo número de secuencia
+    let nextSequence = 1;
+    if (listResult.Contents && listResult.Contents.length > 0) {
+      const sortedContents = listResult.Contents.sort((a, b) => {
+        const aNum = parseInt(a.Key!.split('/').pop()!.split('.')[0]);
+        const bNum = parseInt(b.Key!.split('/').pop()!.split('.')[0]);
+        return aNum - bNum;
+      });
+      const lastFile = sortedContents[sortedContents.length - 1].Key;
+      const lastSequence = parseInt(lastFile!.split('/').pop()!.split('.')[0]);
+      nextSequence = lastSequence + 1;
+    }
+
+    console.log(`Carpeta seleccionada: ${folder}, Próximo número de secuencia: ${nextSequence}`);
+    console.log('Cambiado')
+    // Formatear el nombre del archivo
+    const fileNameParts = customName.split('.');
+    const extension = fileNameParts.pop()?.toLowerCase();
+    const nameWithoutExtension = fileNameParts.join('.').toUpperCase();
+    customName = `${nextSequence}.${nameWithoutExtension}.${extension}`;
+    const key = `${folder}/${customName}`;
+
+    const params = {
+      Bucket: bucketName,
+      Key: key,
+      Body: Buffer.from(buffer),
+      ContentType: file.type,
+    };
+
+    console.log('Iniciando carga a S3');
+    const result = await s3.upload(params).promise();
+    console.log('Carga a S3 completada', result);
+
+    return NextResponse.json({
+      success: true,
+      message: "Video subido exitosamente",
+      data: { key: result.Key, url: result.Location },
+    });
+  } catch (error) {
+    console.error('Error al subir el video:', error);
+        console.log('Cambiado')
+    return NextResponse.json({ success: false, message: "Error al subir el video" }, { status: 500 });
+  }
+}
