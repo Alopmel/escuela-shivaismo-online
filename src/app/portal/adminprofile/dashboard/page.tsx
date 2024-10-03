95b098c783f9f9a35c27bad41a47204b3578d641
Alopmel <78558722+Alopmel@users.noreply.github.com>
2024-10-03 22:50:49 +0200

    test upload S3

--- src/app/portal/adminprofile/dashboard/page.tsx
+++ src/app/portal/adminprofile/dashboard/page.tsx
@@ -1,9 +1,10 @@
-import { AdminDashboard } from "@/ui/portal/adminprofile/admin-dashboard";
+//import { AdminDashboard } from "@/ui/portal/adminprofile/admin-dashboard";
 import { roboto } from "@/app/fonts";
+import FileUpload from "@/ui/portal/adminprofile/FileUpload";
 export default function Dashboard() {
     return (
         <div className={`flex justify-center items-center min-h-screen w-full ml-5 mr-5 mt-10 md:ml-20 md:mr-20 md:mt-40 ${roboto.className}`}>
-            <AdminDashboard />
+            <FileUpload />
         </div>
     );
 }
