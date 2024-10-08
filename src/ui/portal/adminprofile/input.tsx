8dafad777b3d3de661ff017d98e3c4407ff4e7dd
Alopmel <78558722+Alopmel@users.noreply.github.com>
2024-10-08 15:18:53 +0200

    Added inside css Input

--- src/ui/portal/adminprofile/input.tsx
+++ src/ui/portal/adminprofile/input.tsx
@@ -1,36 +1,30 @@
 import React from "react"
 import { roboto } from "@/app/fonts";
-import styles from './AdminDashboard.module.css';
 import { IoPencil } from "react-icons/io5";
 import { FaFolderPlus } from "react-icons/fa6";
 
-export interface InputProps
-  extends React.InputHTMLAttributes<HTMLInputElement> {
+interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
   icon?: 'folder-plus' | 'pencil';
+  label?: string;
 }
 
-const Input = React.forwardRef<HTMLInputElement, InputProps>(
-  ({ className, type, icon, ...props }, ref) => {
-    return (
-      <div className={`${styles.inputWrapper} ${icon ? styles.withIcon : ''}`}>
-        <div className={styles.inputContainer}>
-          <input
-            type={type}
-            className={`${styles.input} ${roboto.className} ${className || ''}`}
-            ref={ref}
-            {...props}
-          />
-        </div>
+export const Input: React.FC<InputProps> = ({ className, type, icon, label, ...props }) => {
+  return (
+    <div className="mb-4">
+      {label && <label className="block text-white mb-2">{label}</label>}
+      <div className="relative">
+        <input
+          type={type}
+          className={`w-full h-14 bg-white bg-opacity-20 border-none rounded-lg px-4 py-2 text-white text-base box-border ${roboto.className} ${className || ''}`}
+          {...props}
+        />
         {icon && (
-          <div className={styles.inputIcon}>
+          <span className="absolute right-3 top-1/2 transform -translate-y-1/2 text-white">
             {icon === 'folder-plus' && <FaFolderPlus />}
             {icon === 'pencil' && <IoPencil />}
-          </div>
+          </span>
         )}
       </div>
-    )
-  }
-)
-Input.displayName = "Input"
-
-export { Input }
+    </div>
+  )
+}
